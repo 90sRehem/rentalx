b71b48466a78db2bd2ee93895520d6493ac8c7d8
Jonathan Rehem <jonathan.de.oliveira@live.com>
2021-06-20 18:06:35 -0300

    update to fit swagger

--- src/modules/cars/useCases/ImportCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/ImportCategory/ImportCategoryController.ts
@@ -4,14 +4,14 @@ import { container } from 'tsyringe';
 import { ImportCategoryService } from './ImportCategoryService';
 
 export class ImportCategoryController {
-    // eslint-disable-next-line prettier/prettier
-    async handle(request: Request, response: Response): Promise<Response> {
-        const importCategoryService = container.resolve(ImportCategoryService);
+  // eslint-disable-next-line prettier/prettier
+  async handle(request: Request, response: Response): Promise<Response> {
+    const importCategoryService = container.resolve(ImportCategoryService);
 
-        const { file } = request;
+    const { file } = request;
 
-        await importCategoryService.execute(file);
+    await importCategoryService.execute(file);
 
-        return response.send();
-    }
+    return response.status(201).send();
+  }
 }
--- src/modules/cars/useCases/ImportCategory/ImportCategoryService.ts
+++ src/modules/cars/useCases/ImportCategory/ImportCategoryService.ts
@@ -5,53 +5,50 @@ import { inject, injectable } from 'tsyringe';
 import { ICategoryRepository } from '../../repositories/ICategoriesRepository';
 
 interface IImportCategory {
-    name: string;
-    description: string;
+  name: string;
+  description: string;
 }
 
 @injectable()
 export class ImportCategoryService {
-    // eslint-disable-next-line prettier/prettier
-    constructor(
-        @inject('CategoriesRepository')
-        private categoriesRepository: ICategoryRepository,
-    ) { }
+  constructor(
+    @inject('CategoriesRepository')
+    private categoriesRepository: ICategoryRepository,
+  ) { }
 
-    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
-        return new Promise((resolve, reject) => {
-            const categories: IImportCategory[] = [];
-            const stream = fs.createReadStream(file.path);
-            const parseFile = csvParse();
-            stream.pipe(parseFile);
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const categories: IImportCategory[] = [];
+      const stream = fs.createReadStream(file.path);
+      const parseFile = csvParse();
+      stream.pipe(parseFile);
 
-            parseFile
-                .on('data', async line => {
-                    const [name, description] = line;
-                    categories.push({
-                        name,
-                        description,
-                    });
-                })
-                .on('end', () => {
-                    fs.promises.unlink(file.path);
-                    resolve(categories);
-                })
-                .on('error', error => {
-                    reject(error);
-                });
+      parseFile
+        .on('data', async line => {
+          const [name, description] = line;
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on('end', () => {
+          fs.promises.unlink(file.path);
+          resolve(categories);
+        })
+        .on('error', error => {
+          reject(error);
         });
-    }
+    });
+  }
 
-    async execute(file: Express.Multer.File): Promise<void> {
-        const categories = await this.loadCategories(file);
-        categories.map(async ({ name, description }) => {
-            const categoryExists = await this.categoriesRepository.findByName(
-                name,
-            );
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+    categories.map(async ({ name, description }) => {
+      const categoryExists = await this.categoriesRepository.findByName(name);
 
-            if (!categoryExists) {
-                await this.categoriesRepository.create({ name, description });
-            }
-        });
-    }
+      if (!categoryExists) {
+        await this.categoriesRepository.create({ name, description });
+      }
+    });
+  }
 }
